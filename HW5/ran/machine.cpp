8d230a5ae75b7f3e2401c6dc326f2af07da5c644
ransoor2 <ransoor2@gmail.com>
2018-06-15 17:25:26 +0300

    f

--- HW5/ran/machine.cpp
+++ HW5/ran/machine.cpp
@@ -5,17 +5,35 @@
 #include <string>
 using namespace std;
 
+#define $RA "$ra"
+#define $SP "$sp"
+#define $SP_ADDRSS "($sp)"
 
-#ifndef CODE
-#define CODE (CodeBuffer::instance())
-#endif
-
-RegisterManager manager;
-
+RegisterManager mgr;
+Assembly asem;
 
 int main(){
+	string reg1 = mgr.getRegAndPromote();
+	string reg2 = mgr.getRegAndPromote();
+	string reg3 = mgr.getRegAndPromote();
 	
-	std::cout << manager.getRegAndPromote() << std::endl;d
-	std::cout << manager.getRegAndPromote() << std::endl;
+	mgr.storeToRegImm(reg1, 5);
+	mgr.storeToRegImm(reg2, 4);
+	mgr.storeToRegImm(reg3, 3);
+	
+	asem.add(reg1,reg2);
+	asem.divide(reg1,reg2);
+	asem.multiply(reg1,reg2);
+	asem.sub(reg1,reg2, false);
+	
+	mgr.freeCurrReg();
+	mgr.freeCurrReg();
+	
+	
+	
+	
+	mgr.print();
+	//std::cout << mgr.getRegAndPromote() << std::endl;
+	//std::cout << mgr.getRegAndPromote() << std::endl;
 }
 
--- HW5/ran/machine.hpp
+++ HW5/ran/machine.hpp
@@ -1,12 +1,22 @@
+#pragma once
+#ifndef MACHINE_HPP
+#define MACHINE_HPP
+
 #include <vector>
 #include <string>
 #include <assert.h>
 #include <iostream>
 #include <sstream>
+#include "bp.hpp"
 using namespace std;
 
+#define BP (CodeBuffer::instance())
 #define FIRST_REGISTER (25)
 #define LAST_REGISTER (8)
+#define $RA "$ra"
+#define $SP "$sp"
+#define $SP_ADDRSS "($sp)"
+#define DEBUG
 
 string toString(int num)
 {
@@ -15,22 +25,33 @@ string toString(int num)
     return os.str();
 }
 
-void my_assert(bool condition){
-	#ifdef MY_DEBUG_FLAG
-	if(!assertion) {
-		cout << "assertion failed" << endl;
-		CODE.printCodeBuffer();sdds
+void ASSERT(bool condition){
+	#ifdef DEBUG
+	if(!condition) {
+		cout << "\033[1;31m ASSERT FAILED \033[0m" << endl;
+		BP.printCodeBuffer();
 	}
 	assert(condition);
 	#endif
 }
 
+void ASSERT_REG(int reg) {
+	if (reg <= FIRST_REGISTER && reg >= LAST_REGISTER) return;
+	cout << "\033[1;31m failed on reg $\033[0m" << reg << endl;
+	ASSERT(false);
+}
 
 class RegisterManager{
 	int currentReg; 
+	void doRegOp(string op, string left, string right){
+		BP.emit(op + " " + left + ", " + right);
+	}
 public:
 	RegisterManager(){
 		currentReg = FIRST_REGISTER;
+	}
+	~RegisterManager(){
+		//BP.printCodeBuffer();
 	}
 	string getCurrReg() {
 		return "$" + toString(currentReg);
@@ -38,7 +59,78 @@ public:
 	string getRegAndPromote(){
 		string ret = getCurrReg();
 		currentReg--;
+		ASSERT_REG(currentReg);
 		return ret;
+	}
+	void freeCurrReg(){
+		currentReg++;
+		ASSERT_REG(currentReg);
+	}
+	void storeFromReg(string reg, string dest){
+		doRegOp("sw", reg, dest);		
+	}
+	void storeToReg(string reg, string source){
+		doRegOp("lw", reg, source);		
+	}
+	void storeToRegImm(string reg, int value){
+		doRegOp("li", reg, toString(value));		
+	}
+	
+	
+	
+	void print(){
+		BP.printCodeBuffer();
 	}
 };
 
+
+class Assembly {
+public:
+	Assembly(){};
+	void multiply(string regLeftRes, string regRight){
+		BP.emit("mult " + regLeftRes + "," + regRight);
+		BP.emit("mflo " + regLeftRes);
+	}
+	void divide(string regLeftRes, string regRight){
+		//TODO - check for DIVIEBYZERO
+		BP.emit("div " + regLeftRes + "," + regRight);
+		BP.emit("mflo " + regLeftRes);
+	}
+	void sub(string regLeftRes, string regRight, bool isB){
+		if (isB) {
+			BP.emit("addiu " + regLeftRes + ", " + regLeftRes + ", 256");
+		}
+		BP.emit("subu " + regLeftRes + "," + regLeftRes + "," + regRight);
+	}
+	void add(string regLeftRes, string regRight){
+		BP.emit("addu " + regLeftRes + ","+regLeftRes+","+regRight);
+	}
+	
+	
+	
+	
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
